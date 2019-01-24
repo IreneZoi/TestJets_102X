8fab1ab1c173fb3cb79aebec01d47d763c321d33
Irene <irene.zoi@cern.ch>
2019-01-24 13:51:47 +0100

    cleaning code after trying to add other selections

--- include/Plot94JetsSelections.h
+++ include/Plot94JetsSelections.h
@@ -10,15 +10,13 @@ namespace uhh2examples {
  * maximum third jet pt fraction can be changed in the constructor.
  * The jets are assumed to be sorted in pt.
  */
-class DijetSelection: public uhh2::Selection {
-public:
+  class DijetSelection: public uhh2::Selection {
+  public:
     DijetSelection(float dphi_min = 2.7f, float third_frac_max = 0.2f);
     virtual bool passes(const uhh2::Event & event) override;
-private:
+  private:
     float dphi_min, third_frac_max;
-};
-
-
+  };
 
 
 }
--- src/Plot94JetsSelections.cxx
+++ src/Plot94JetsSelections.cxx
@@ -8,7 +8,7 @@ using namespace uhh2;
 
 
 DijetSelection::DijetSelection(float dphi_min_, float third_frac_max_): dphi_min(dphi_min_), third_frac_max(third_frac_max_){}
-    
+
 bool DijetSelection::passes(const Event & event){
     assert(event.jets); // if this fails, it probably means jets are not read in
     if(event.jets->size() < 2) return false;
@@ -21,7 +21,4 @@ bool DijetSelection::passes(const Event & event){
     auto third_jet_frac = jet2.pt() / (0.5 * (jet0.pt() + jet1.pt()));
     return third_jet_frac < third_frac_max;
 }
-
-
-
 
